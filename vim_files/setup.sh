5e4512eccae6bfa9123779a2b35717f1a002952b
Hans Elliott <hanselliott61@gmail.com>
2023-07-03 16:32:57 +0000

    reorg

--- vim_files/setup.sh
+++ vim_files/setup.sh
@@ -2,11 +2,10 @@
 
 # INSTALL PLUGINS
 # Plugins installed into this directory will be autoloaded upon vim startup
+cd ./vim/pack/vendor/start 
 
 # vim-slime - send code from vim to terminal
 git clone https://github.com/jpalardy/vim-slime.git
 
 # NERDTree - file tree browser
 git clone --depth 1 https://github.com/preservim/nerdtree.git
-
-
